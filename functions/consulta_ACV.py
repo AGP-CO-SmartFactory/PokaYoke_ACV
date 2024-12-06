09d4ecf27a349ba817969c863306203775c5ee24
DiegoBareno <dbareno@agpglass.com>
2024-12-06 17:29:20 -0500

    Salida logica para estado ACV
    funciones que determinan si una autoclave esta en estado inactivo

--- functions/consulta_ACV.py
+++ functions/consulta_ACV.py
@@ -2,6 +2,28 @@ from functions.sql_utilities import SqlUtilities
 import pandas as pd
 
 
-# Esta función debe leer un registro del PLC o de una base de datos de mantenimiento para retornar un estado para cada Autoclave
+# Esta clase lee desde la base de datos, el estado de la ultima receta de las ACV
 # Puede retornar un dataframe con valores booleanos
 
+
+class Acv:
+
+    def __init__(self):
+        query = """SELECT * 
+        FROM SF_CiclosLaminado WITH(NOLOCK)
+        """
+        self.estados_acv = SqlUtilities.get_database_sf(query)
+        # print(self.estados_acv) #Confirmación
+
+    def estado_acv(self):
+        acv_ultimo_estado = self.estados_acv.loc[
+            self.estados_acv.groupby("ID_acv")["Hora"].idxmax()
+        ]
+        acv_ultimo_estado["INACTIVO"] = (acv_ultimo_estado["Evento"] == "Final") & (
+            acv_ultimo_estado["Etapa"] == "Finalizado"
+        )
+        acv_ultimo_estado = acv_ultimo_estado.drop(
+            columns=["ID", "Consecutivo", "Evento", "Ciclo"], axis=0, inplace=False
+        )
+        # print(ultimo_estado) #Confirmación
+        return acv_ultimo_estado
