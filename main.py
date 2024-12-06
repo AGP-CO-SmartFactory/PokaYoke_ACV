f365fa6790a61d4e5c2b0aff4fe9329ab0bd2a99
DiegoBareno <dbareno@agpglass.com>
2024-12-06 17:26:30 -0500

    Inicialización de opciones

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import sys
+from functions.consulta_ACV import Acv
+from functions.estado_desaire import EstadoPiezas
 
-
+def main(opcion):
+    if opcion == "A":
+        self=Acv()
+        Acv.estado_acv(self)   
+    elif opcion =="B":
+        self=EstadoPiezas() 
+        EstadoPiezas.tratamiento_datos(self)
+    else:
+        print("Opción no válida.")
+        
+if __name__ == "__main__":
+    opcion = sys.argv[1]
+    main(opcion)
