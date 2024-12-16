18ba209f128ee00ec1941e7f73dde9999d44c743
DiegoBareno <dbareno@agpglass.com>
2024-12-16 11:40:24 -0500

    funcion creada
    Se crea la función para minimizar la cantidad de piezas para verificar en un día la cantidad de registros de embolsado/desaireación y encontrar inconsistencias

---
+++ functions/verificar_historico_embolsado.py
@@ -0,0 +1,51 @@
+from functions.sql_utilities import SqlUtilities
+import pandas as pd
+import warnings 
+
+
+warnings.filterwarnings('ignore')
+
+
+#Este código existe con la función de poder verificar por días que las ordenes tengan
+#tanto registro de embolsado como registro de desaireación, esto debido a que se presentaron
+#no conformidades en el proceso que dañaban la lógica para el bloqueo de autoclaves
+
+class Cumplimiento_registro_embolsados:
+
+    def __init__(self):
+        query_historico = """
+                    SELECT 
+                        CE.ORDEN, 
+                        CE.PTO_TRABAJO, 
+                        CE.CLV_MODEL, 
+                        CE.DATE_NOTIF,
+                        CE.HRA_NOTIF
+                    FROM 
+                        [Comercial].[dbo].[VW_CAMBIOESTADO] AS CE WITH (NOLOCK)
+                    WHERE 
+                        CE.DATE_NOTIF = '20241213'
+                        AND CE.PTO_TRABAJO = '15EMBOL'
+                        AND CE.TIPO_NOTIF = 'APROBADO'
+                 """
+        self.historico_embolsado = SqlUtilities.get_database_com(query_historico)
+    
+    def filtrar_keymodel(self):
+        self.filtro_embolsa = self.historico_embolsado[self.historico_embolsado['CLV_MODEL'] == 'EMBOLSA']
+        self.filtro_desaire = self.historico_embolsado[self.historico_embolsado['CLV_MODEL'] == 'DESAIRE']
+
+    def criterio_registro_completo(self):
+        self.ordenes_embolsa = set(self.filtro_embolsa['ORDEN'])
+        self.ordenes_desaire = set(self.filtro_desaire['ORDEN'])
+        self.ordenes_validas = self.ordenes_embolsa & self.ordenes_desaire
+
+    def crear_df_ordenes_resultados(self):
+        self.df_ordenes_validas = self.historico_embolsado[self.historico_embolsado['ORDEN'].isin(self.ordenes_validas)]
+        self.df_ordenes_invalidas = self.historico_embolsado[~self.historico_embolsado['ORDEN'].isin(self.ordenes_validas)]
+        
+    def ejecutar_revision(self):
+        self.filtrar_keymodel()
+        self.criterio_registro_completo()
+        self.crear_df_ordenes_resultados()
+        return self.df_ordenes_validas, self.df_ordenes_invalidas
+
+
--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import sys
 from functions.consulta_ACV import Acv
-from functions.estado_desaire import EstadoPiezas
+from functions.estado_desaire import EstadoPiezas, AlarmaDesaireacion
+from functions.verificar_historico_embolsado import Cumplimiento_registro_embolsados
 
 def main(opcion):
-    if opcion == "A":
+    if opcion == "EstadoACV":
         self=Acv()
         Acv.estado_acv(self)   
 
-    elif opcion =="B":
+    elif opcion =="EstadoPiezas":
         self=EstadoPiezas() 
         EstadoPiezas.tratamiento_datos(self)
         EstadoPiezas.cargar_datos_sql(self)
+    
+    elif opcion =="NoConformidades":
+        self = AlarmaDesaireacion()
+        AlarmaDesaireacion.filtrar_no_conformidades(self)
+    
+    elif opcion == 'DobleRegistro':
+
+        self = Cumplimiento_registro_embolsados()
+        validas, invalidas = Cumplimiento_registro_embolsados.ejecutar_revision(self)
+
+        invalidas.to_excel('invalidas.xlsx', index= False)
+
+
     else:
         print("Opción no válida.")
+    
+
         
 if __name__ == "__main__":
     opcion = sys.argv[1]
