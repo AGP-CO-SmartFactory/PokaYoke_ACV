993a56152d99ca5a9d7d7f7c44dea71282b9153e
DiegoBareno <dbareno@agpglass.com>
2024-12-23 15:22:52 -0500

    Rutina inicio de app
    Se modifica la lógica del inicio de sap para prevenir que se ejecuten las acciones antes de que sap estuviese abierto

--- functions/notificacion_auto_sap.py
+++ functions/notificacion_auto_sap.py
@@ -1,26 +1,56 @@
 import pandas as pd
 import win32com.client #Para acceder a SAP y ejecuta el script
-from datetime import datetime
-import subprocess
+import psutil #Para verificar apps abiertas
+#from datetime import datetime
+#import subprocess
 import os
+import time
 
 #Esta clase se encarga de realizar la lectura de la base de datos transicional resultante de
 #la powerapp de desaireación, para luego ejecutar de manera automática todas las notificaciones
 #automaticas de las ordenes para el puesto de trabajo 15EMBOL KeyModel DESAIRE.
 
 class auto_sap():
-
-    def start_sap(self, sap_user, sap_psw):
-        os.startfile(r'C:\Program Files (x86)\SAP\FrontEnd\SapGui\saplogon.exe') #Buscar ruta SAP pc destino
+    
+    def sap_app_verification(self):
+        for process in psutil.process_iter(['name']):
+            if process.info['name'] and 'saplogon.exe'.lower() in process.info['name'].lower():
+                return True
+        return False
+    
+    def start_sap(self):
+        if self.sap_app_verification():
+            os.startfile(r'C:\Program Files (x86)\SAP\FrontEnd\SapGui\saplogon.exe') #Buscar ruta SAP pc destino
+        else:
+            os.startfile(r'C:\Program Files (x86)\SAP\FrontEnd\SapGui\saplogon.exe') #Buscar ruta SAP pc destino
+            time.sleep(10)
 
     def sap_connection(self):
         SapGuiAuto = win32com.client.GetObject('SAPGUI')
         application = SapGuiAuto.GetScriptingEngine
         connection = application.Children(0)
-        session = connection.Children(0)
-        return session
+        self.session = connection.Children(0)
+        return self.session
         #Esta función retorna un objeto que da acceso de todas las ventanas abiertas de sap al script
-        
 
+    def consulta_fin_turno(self):
+        self.session.findById("wnd[0]").maximize()
+        self.session.findById("wnd[0]/usr/cntlIMAGE_CONTAINER/shellcont/shell/shellcont[0]/shell").selectedNode = "F00019"
+        self.session.findById("wnd[0]/usr/cntlIMAGE_CONTAINER/shellcont/shell/shellcont[0]/shell").doubleClickNode("F00019")
+        self.session.findById("wnd[0]/usr/ctxtP_WERKS-LOW").text = "CO01"
+        self.session.findById("wnd[0]/usr/ctxtP_AUFNR-LOW").text = "11048497"
+        self.session.findById("wnd[0]/usr/ctxtP_AUFNR-LOW").setFocus()
+        self.session.findById("wnd[0]/usr/ctxtP_AUFNR-LOW").caretPosition = 8
+        self.session.findById("wnd[0]/tbar[1]/btn[8]").press()
+        self.session.findById("wnd[0]/tbar[0]/btn[3]").press()
+        self.session.findById("wnd[0]/tbar[0]/btn[3]").press()
+        self.session.findById("wnd[0]").close()
+        self.session.findById("wnd[1]/usr/btnSPOP-OPTION1").press()
+
+    
     def ejecutar(self):
-        self.start_sap(sap_user = 'DBARENO', sap_psw = 'Dibar461....')
+        self.start_sap()
+        self.sap_connection()
+        self.consulta_fin_turno()
+    
+
