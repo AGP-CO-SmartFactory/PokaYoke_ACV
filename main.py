e810963a4d7a77a298460284b30ef56abf6b7709
DiegoBareno <dbareno@agpglass.com>
2024-12-23 12:25:28 -0500

    Version 1.0
    Estado Piezas: Ya realiza la consulta del estado de las piezas correctamente para todas sus particularidades.
    
    No conformidades: se añade la función para enviar correos cuando hay NC
    
    inicio de cargue SAP.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 from functions.consulta_ACV import Acv
 from functions.estado_desaire import EstadoPiezas, AlarmaDesaireacion
 from functions.verificar_historico_embolsado import Cumplimiento_registro_embolsados
+from functions.send_email import SendEmail
+from functions.notificacion_auto_sap import auto_sap
 
 def main(opcion):
     if opcion == "EstadoACV":
@@ -10,26 +12,24 @@ def main(opcion):
 
     elif opcion =="EstadoPiezas":
         self=EstadoPiezas() 
-        estado = EstadoPiezas.tratamiento_datos(self)
-        estado.to_excel("prueba.xlsx", index=False)
+        EstadoPiezas.tratamiento_datos(self)
         EstadoPiezas.cargar_datos_sql(self)
     
     elif opcion =="NoConformidades":
         self = AlarmaDesaireacion()
-        AlarmaDesaireacion.filtrar_no_conformidades(self)
+        tabla_nc = AlarmaDesaireacion.filtrar_no_conformidades(self)
+        if not tabla_nc.empty:
+            SendEmail.mail_nc_acv(tabla_nc)
     
     elif opcion == 'DobleRegistro':
-
         self = Cumplimiento_registro_embolsados()
         validas, invalidas = Cumplimiento_registro_embolsados.ejecutar_revision(self)
         invalidas.to_excel('invalidas.xlsx', index= False)
-        validas.to_excel('validas.xlsx', indes= False)
-
-
-    else:
-        print("Opción no válida.")
+        validas.to_excel('validas.xlsx', index= False)
     
-
+    elif opcion == 'SAP':
+        self = auto_sap()
+        auto_sap.ejecutar(self)
         
 if __name__ == "__main__":
     opcion = sys.argv[1]
