c6061e8cdeab8f7f8cfb8b7e5ead2df51c812c78
DiegoBareno <dbareno@agpglass.com>
2024-12-23 12:22:25 -0500

    Función EMAIL
    Se añade la función modificada para enviar emails según tiempos de desaireación NC

--- functions/send_email.py
+++ functions/send_email.py
@@ -4,8 +4,9 @@ import smtplib
 
 class SendEmail:
 
-    def enviar_mail_h2o_faltante(tabla_faltantes):
+    def mail_nc_acv(tabla_nc):
 
+        tabla_nc = tabla_nc.to_html(index=False, border=1, justify="center")
         print("Enviando correo...")
         remitente = "pcplanta@agpglass.com"
 
@@ -35,10 +36,10 @@ class SendEmail:
         </head>
         <body>
         <div class="container">
-            <h1>Seguimiento piezas sin H2o AGP sGlass</h1>
-            <p>Se ha detectado las siguientes piezas aprobadas en UMA4 no tienen registro de H2o (Y lo requiere):</p>
+            <h1>Seguimiento NC desaireación AGP sGlass</h1>
+            <p>Se ha detectado las siguientes piezas con desaireación aprobada que no cumple con los criterios establecidos:</p>
             <p>Detalles:</p>
-            {tabla_faltantes} 
+            {tabla_nc} 
             <p>Esto es un correo enviado automaticamente. Atentamente equipo de Smart Factory AGPsglass Colombia</p>
         </div>
         </body>
@@ -46,119 +47,10 @@ class SendEmail:
         """
         email = EmailMessage()
         email["From"] = remitente
-        destinatario = ["dbareno@agpglass.com", "opinto@agpglass.com","xarodriguez@agpglass.com", "magonzalez@agpglass.com", "fcastillo@agpglass.com","afgarcia@agpglass.com"]
-        email["Subject"] = "Alerta NO H2o UMA 4"
-        email.add_alternative(mensaje, subtype="html")
-
-        smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
-        smtp.starttls()
-        smtp.login(remitente, "AGPglass2021")
-        smtp.sendmail(remitente, destinatario, email.as_string())
-        smtp.quit()
-        print("Email Enviado con exito..")
-
-    def enviar_mail_h2o_incoming(tabla_faltantes):
-
-        print("Enviando correo...")
-        remitente = "pcplanta@agpglass.com"
-
-        mensaje = f"""
-        <html>
-        <head>
-        <style>
-        /* Estilos personalizados para el correo */
-        body {{
-            font-family: Arial, sans-serif;
-            background-color: #f2f2f2;
-            padding: 20px;
-        }}
-        .container {{
-            background-color: #fff;
-            border-radius: 10px;
-            padding: 20px;
-        }}
-        h1 {{
-            color: #333;
-        }}
-        .alert {{
-            color: #ff0000;
-            font-weight: bold;
-        }}
-        </style>
-        </head>
-        <body>
-        <div class="container">
-            <h1>Seguimiento piezas entrantes UMA4 AGP sGlass</h1>
-            <p>Se ha detectado las siguientes piezas que van a llegar proximamente a UMA4:</p>
-            <p>Detalles:</p>
-            {tabla_faltantes} 
-            <p> Si existen valores negativos en ENTH puede indicar que la pieza no tuvo registro en Salida de hornos (SALH) o es un indicativo de que el tiempo de ciclo fue mayor al esperado máximo (23 horas)</p>
-            <p> Para valores negativos en SALH significa que es una pieza que no ha bajado de hornos o no se ha retenido a la entrada UMA4 </p>
-            <p> Este correo aún no incluye las piezas que estén en la directriz 34, no avisa de recuperaciones de lites ni cambios tapa/pro </p>
-            
-            <p>Esto es un correo enviado automaticamente. Atentamente equipo de Smart Factory AGPsglass Colombia</p>
-
-        </div>
-        </body>
-        </html>
-        """
-        email = EmailMessage()
-        email["From"] = remitente
-        destinatario = ["dbareno@agpglass.com", "opinto@agpglass.com","xarodriguez@agpglass.com", "magonzalez@agpglass.com", "fcastillo@agpglass.com","afgarcia@agpglass.com"]
-        email["Subject"] = "Segumiento ingresos H2o Uma4"
-        email.add_alternative(mensaje, subtype="html")
-
-        smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
-        smtp.starttls()
-        smtp.login(remitente, "AGPglass2021")
-        smtp.sendmail(remitente, destinatario, email.as_string())
-        smtp.quit()
-        print("Email Enviado con exito..")
-
-    def enviar_mail_gasp_faltante(tabla_faltantes):
-
-        print("Enviando correo...")
-        remitente = "pcplanta@agpglass.com"
-
-        mensaje = f"""
-        <html>
-        <head>
-        <style>
-        /* Estilos personalizados para el correo */
-        body {{
-            font-family: Arial, sans-serif;
-            background-color: #f2f2f2;
-            padding: 20px;
-        }}
-        .container {{
-            background-color: #fff;
-            border-radius: 10px;
-            padding: 20px;
-        }}
-        h1 {{
-            color: #333;
-        }}
-        .alert {{
-            color: #ff0000;
-            font-weight: bold;
-        }}
-        </style>
-        </head>
-        <body>
-        <div class="container">
-            <h1>Seguimiento piezas sin medición GASP AGP sGlass</h1>
-            <p>Se ha detectado las siguientes piezas aprobadas en UMA4 no tienen registro de Esfuerzos (GASP) (Y lo requiere):</p>
-            <p>Detalles:</p>
-            {tabla_faltantes} 
-            <p>Esto es un correo enviado automaticamente. Atentamente equipo de Smart Factory AGPsglass Colombia</p>
-        </div>
-        </body>
-        </html>
-        """
-        email = EmailMessage()
-        email["From"] = remitente
-        destinatario = ["dbareno@agpglass.com"]#, "dpinilla@agpglass.com"]#, "salvarez@agpglass.com"]
-        email["Subject"] = "Alerta NO GASP X5"
+        destinatario = ["dbareno@agpglass.com", "ltovar@agpglass.com", "ebejarano@agpglass.com", "wrubio@agpglass.com", 
+                        "jogomez@agpglass.com", "xarodriguez@agpglass.com", "afgarcia@agpglass.com", "opinto@agpglass.com", 
+                        "fcastillo@agpglass.com"]
+        email["Subject"] = "Alerta NC Desaireacion ACV"
         email.add_alternative(mensaje, subtype="html")
 
         smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
